b3a0a2604c00ae486d2c5bdf61bd18be3754d798
Lingsi Ke <lingsike@Lingsis-MacBook-Pro.local>
2017-08-19 21:21:24 -0700

    LoginAndReg

---
+++ src/main/java/com/example/auth/controllers/Users.java
@@ -0,0 +1,71 @@
+package com.example.auth.controllers;
+
+import java.security.Principal;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.auth.models.User;
+import com.example.auth.services.UserService;
+import com.example.auth.validator.UserValidator;
+
+@Controller
+public class Users {
+    private UserService userService;
+    private UserValidator userValidator;
+    
+    public Users(UserService userService, UserValidator userValidator) {
+        this.userService = userService;
+        this.userValidator = userValidator;
+    }
+    
+    
+//    @RequestMapping("/registration")
+//    public String registerForm(@Valid @ModelAttribute("user") User user) {
+//        return "registrationPage.jsp";
+//    }
+    
+    @PostMapping("/registration")
+    public String registration(@Valid @ModelAttribute("user") User user, BindingResult result, Model model) {
+        userValidator.validate(user, result);
+        if (result.hasErrors()) {
+            return "loginPage.jsp";
+        }
+        
+        userService.saveWithUserRole(user);
+        return "redirect:/";
+    }
+    
+    
+    @RequestMapping("/login")
+    public String login(@Valid @ModelAttribute("user") User user, @RequestParam(value="error", required=false) String error, @RequestParam(value="logout", required=false) String logout, Model model) {
+        if(error != null) {
+            model.addAttribute("errorMessage", "Invalid Credentials, Please try again.");
+        }
+        if(logout != null) {
+            model.addAttribute("logoutMessage", "Logout Successfull!");
+        }
+        return "loginPage.jsp";
+    }
+    
+    @RequestMapping("/admin")
+    public String adminPage(Principal principal, Model model) {
+        String username = principal.getName();
+        model.addAttribute("currentUser", userService.findByUsername(username));
+        return "adminPage.jsp";
+    }
+    
+    @RequestMapping(value = {"/", "/home"})
+    public String home(Principal principal, Model model) {
+        String username = principal.getName();
+        model.addAttribute("currentUser", userService.findByUsername(username));
+        return "homePage.jsp";
+    }
+}
